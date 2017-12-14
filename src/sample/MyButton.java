a6fbcb410a31cbc6241fee00e75c8177edc1c3a4
Valentin Nourdin <valentin.nourdin@hotmail.fr>
2017-12-14 11:09:35 +0100

    Nicer buttons

--- src/sample/MyButton.java
+++ src/sample/MyButton.java
@@ -11,8 +11,19 @@ public class MyButton extends Button {
     public MyButton(String text) {
         super(text);
         this.setTextFill(Color.DARKRED);
-        this.setBorder(new Border(new BorderStroke(Color.GREY, BorderStrokeStyle.SOLID, new CornerRadii(0), new BorderWidths(4,4,4,4))));
-        this.setBackground(new Background(new BackgroundFill(Color.LIGHTBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
+        this.setBorder(new Border(new BorderStroke(Color.GRAY, BorderStrokeStyle.SOLID, new CornerRadii(0), new BorderWidths(4, 4, 4, 4))));
         this.setFont(new Font(25));
+
+        this.setNormalBackground();
+        this.setOnMouseEntered(mouseEntered -> this.setHoverBackground());
+        this.setOnMouseExited(mouseExited -> this.setNormalBackground());
+    }
+
+    private void setNormalBackground() {
+        this.setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, CornerRadii.EMPTY, Insets.EMPTY)));
+    }
+
+    private void setHoverBackground() {
+        this.setBackground(new Background(new BackgroundFill(Color.LIGHTBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
     }
 }
