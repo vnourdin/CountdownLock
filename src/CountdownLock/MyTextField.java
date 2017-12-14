ce73e436438280fb9f52bd675143f0388ac5ac1d
Valentin Nourdin <valentin.nourdin@hotmail.fr>
2017-12-14 11:54:50 +0100

    Adding visual help to know if words are ok

--- src/CountdownLock/MyTextField.java
+++ src/CountdownLock/MyTextField.java
@@ -1,15 +1,28 @@
 package CountdownLock;
 
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
 public class MyTextField extends TextField {
     private String passphrase;
+    private Background correctBack = BackgroundFactory.getBasic(Color.LIGHTBLUE);
+    private Background incorrectBack = BackgroundFactory.getBasic(Color.INDIANRED);
 
     public MyTextField(String passphrase) {
         super();
         this.passphrase = passphrase;
         this.setFont(new Font(40));
+        this.setBackground(this.incorrectBack);
+        this.setOnKeyReleased(e -> this.actualizeBack());
+    }
+
+    private void actualizeBack() {
+        if (this.isWellFilled())
+            this.setBackground(this.correctBack);
+        else
+            this.setBackground(this.incorrectBack);
     }
 
     public boolean isWellFilled() {
