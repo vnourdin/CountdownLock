f2c93d3b952366215d5701037fd7364acf220099
Valentin Nourdin <valentin.nourdin@hotmail.fr>
2017-12-14 12:13:14 +0100

    MyLabel clean

--- src/CountdownLock/MyLabel.java
+++ src/CountdownLock/MyLabel.java
@@ -8,18 +8,13 @@ public class MyLabel extends Label {
 
     public MyLabel(String text) {
         super(text);
-        this.config();
-    }
-
-    public MyLabel(MyIntegerProperties property) {
-        super();
-        this.config();
-        this.textProperty().bind(property.getStringProperty());
-    }
-
-    private void config() {
         this.setTextFill(Color.DARKRED);
         this.setStyle("-fx-font-size: 10em");
         this.setTextAlignment(TextAlignment.CENTER);
+    }
+
+    public MyLabel(MyIntegerProperties property) {
+        this("");
+        this.textProperty().bind(property.getStringProperty());
     }
 }
