5c07d6da827ec5519f4ef2fc84d5ca789adeee1a
Valentin Nourdin <valentin.nourdin@hotmail.fr>
2017-12-15 16:21:03 +0100

    Css for buttons

--- src/CountdownLock/Generic/MyButton.java
+++ src/CountdownLock/Generic/MyButton.java
@@ -1,30 +1,11 @@
 package CountdownLock.Generic;
 
 import javafx.scene.control.Button;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 
 public class MyButton extends Button {
-    private Background normalBack = BackgroundFactory.get(Color.LIGHTGRAY);
-    private Background hoverBack = BackgroundFactory.get(Color.LIGHTBLUE);
 
     public MyButton(String text) {
         super(text);
-        this.setTextFill(Color.DARKRED);
-        this.setBorder(new Border(new BorderStroke(Color.GRAY, BorderStrokeStyle.SOLID, new CornerRadii(0), new BorderWidths(4, 4, 4, 4))));
-        this.setFont(new Font(25));
-
-        this.setNormalBackground();
-        this.setOnMouseEntered(mouseEntered -> this.setHoverBackground());
-        this.setOnMouseExited(mouseExited -> this.setNormalBackground());
-    }
-
-    private void setNormalBackground() {
-        this.setBackground(this.normalBack);
-    }
-
-    private void setHoverBackground() {
-        this.setBackground(this.hoverBack);
+        this.getStyleClass().add("my-button");
     }
 }
