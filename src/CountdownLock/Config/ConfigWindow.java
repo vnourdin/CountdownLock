99ccbc2127aa22071fc8fb51fae8521e476693c3
Valentin Nourdin <valentin.nourdin@hotmail.fr>
2017-12-16 23:05:12 +0100

    Event handling to start the game

--- src/CountdownLock/Config/ConfigWindow.java
+++ src/CountdownLock/Config/ConfigWindow.java
@@ -1,4 +1,25 @@
 package CountdownLock.Config;
 
+import CountdownLock.Game.GameBox;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+
 public class ConfigWindow {
+    @FXML
+    private GridPane gridPane;
+    @FXML
+    private TextField words;
+    @FXML
+    private Spinner<Integer> timer;
+    @FXML
+    private CheckBox help;
+
+    @FXML
+    protected void handleSubmitButtonAction() {
+        gridPane.getScene().setRoot(new GameBox(words.getText().split(" "), timer.getValue(), help.isSelected()));
+    }
 }
